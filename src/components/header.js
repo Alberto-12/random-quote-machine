14ae7307ceb75e6cfc9ac3ff3f538dd04f828e6a
Alberto Serafini <ilgumo@gmail.com>
2022-12-30 13:11:58 -0500

    worked on the 6th user story

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,40 @@
+import quotes from './quotes';
+import React, { useState } from 'react';
+//User Story #6: On first load, my quote machine displays a random quote in the element with id="text".
+
 const QuoteBox = () => {
+  // this Declares a state variable called "quote" with an initial value of an empty string
+  const [quote, setQuote] = useState('');
+
+  // this Creates a function that sets the quote to a new random quote
+  const getNewQuote = () => {
+    // this Gets a new quote from the quotes array
+    const newQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    // this Updates the quote state with the new quote
+    setQuote(newQuote);
+  }
+
+  // this On first load, gets a new quote
+  React.useEffect(() => {
+    getNewQuote();
+  }, []); // The empty array ensures that this only runs on first load
+
   return (
     <div id="quote-box">
-      <h1>Hello!</h1>
+      <div id="text">
+        {quote.text}
+      </div>
+      <div id="author">
+        author element
+      </div>
+      <button id="new-quote" onClick={getNewQuote}>New Quote</button>
+      <a href="/" id="tweet-quote">Tweet Quote</a>
     </div>
   );
 };
+
+    
+    
 
 export default QuoteBox;
 
---
+++ src/components/quotes.js
@@ -0,0 +1,11 @@
+const quotes = [
+    { text: ' quote text here', author: 'author name here' },
+    { text: 'another quote text here', author: 'another author name here' },
+    // Add more quotes here
+  ];
+  
+  export default quotes;
+  
+  
+  
+  
