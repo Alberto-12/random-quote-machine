d99a59873069881ffe4d2fd5a3e247497278434d
Alberto Serafini <ilgumo@gmail.com>
2023-01-02 15:09:04 -0500

    worked on ths user story 7

--- src/components/header.js
+++ src/components/header.js
@@ -3,35 +3,38 @@ import React, { useState } from 'react';
 //User Story #6: On first load, my quote machine displays a random quote in the element with id="text".
 
 const QuoteBox = () => {
-  // this Declares a state variable called "quote" with an initial value of an empty string
-  const [quote, setQuote] = useState('');
-
-  // this Creates a function that sets the quote to a new random quote
-  const getNewQuote = () => {
-    // this Gets a new quote from the quotes array
-    const newQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    // this Updates the quote state with the new quote
-    setQuote(newQuote);
-  }
-
-  // this On first load, gets a new quote
-  React.useEffect(() => {
-    getNewQuote();
-  }, []); // The empty array ensures that this only runs on first load
-
-  return (
-    <div id="quote-box">
-      <div id="text">
-        {quote.text}
+    // Declare state variables for quote and author
+    const [quote, setQuote] = useState({ text: '', author: '' });
+    const [author, setAuthor] = useState('');
+  
+    // Create a function that sets the quote and author states to new random values
+    const getNewQuote = () => {
+      // Get a new quote from the quotes array
+      const newQuote = quotes[Math.floor(Math.random() * quotes.length)];
+      // Update the quote and author states with the new quote and its author
+      setQuote(newQuote);
+      setAuthor(newQuote.author);
+    }
+  
+    // On first load, get a new quote
+    React.useEffect(() => {
+      getNewQuote();
+    }, []); // The empty array ensures that this only runs on first load
+  
+    return (
+      <div id="quote-box">
+        <div id="text">
+          {quote.text}
+        </div>
+        <div id="author">
+          {author}
+        </div>
+        <button id="new-quote" onClick={getNewQuote}>New Quote</button>
+        <a href="/" id="tweet-quote">Tweet Quote</a>
       </div>
-      <div id="author">
-        author element
-      </div>
-      <button id="new-quote" onClick={getNewQuote}>New Quote</button>
-      <a href="/" id="tweet-quote">Tweet Quote</a>
-    </div>
-  );
-};
+    );
+  };
+  
 
     
     
